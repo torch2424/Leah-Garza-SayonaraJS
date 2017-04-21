9b45e1e671ed597e4d69ff485c0181106b9bfc47
Aaron Turner <aaron@aaronthedev.com>
2017-04-21 01:54:43 -0700

    Currently fixin overlp CSS for navbar

--- src/app/components/main/main.js
+++ src/app/components/main/main.js
@@ -14,6 +14,9 @@ class MainController {
     this.entryHelper = EntryHelper;
 
     this.sayonaraSite = {};
+
+    this.totalCategories = [];
+    this.currentCategory = false;
   }
 
   $onInit() {
@@ -21,6 +24,15 @@ class MainController {
       // Using timeout to propogate controller changes
       this.$timeout(() => {
         this.sayonaraSite = siteJson;
+
+        // Get our entry categories
+        this.sayonaraSite.pages[0].entryTypes[0].entries.forEach(entry => {
+          entry.categories.forEach(category => {
+            if (!this.totalCategories.includes(category.title)) {
+              this.totalCategories.push(category.title);
+            }
+          });
+        });
       }, 0);
     });
   }
@@ -30,6 +42,47 @@ class MainController {
    */
   goToEntry(entry) {
     this.$location.path('/entry/' + entry.title);
+  }
+
+  /**
+   * Funciton to act as our category filter
+   */
+  entryCategoryFilter(entry) {
+    // Check if we have no current category
+    if (!this.currentCategory) {
+      return true;
+    }
+    // Check if we have no categories
+    if (!entry ||
+      !entry.categories ||
+      entry.categories.length <= 0) {
+      return false;
+    }
+    // Look through our entry categories
+    let response = false;
+    entry.categories.some(category => {
+      if (category.title === this.currentCategory) {
+        response = true;
+        return true;
+      }
+      return false;
+    });
+    return response;
+  }
+
+  /**
+   * function called whenever a category is clicked
+   */
+  categoryClick(category) {
+    // First check if we a rediabling a category
+    if (this.currentCategory &&
+      this.currentCategory === category) {
+      this.currentCategory = false;
+      return;
+    }
+
+    // If not, simply assign the category
+    this.currentCategory = category;
   }
 }
 
