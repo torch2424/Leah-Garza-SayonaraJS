16342ec61c828ce476141f2287192bc0a5b291c7
Aaron Turner <aaron@aaronthedev.com>
2017-04-25 09:46:54 -0700

    Fixed the port

--- src/app/services/sayonara.js
+++ src/app/services/sayonara.js
@@ -16,7 +16,11 @@ class Sayonara {
     // Production and API URL from webpack config
     if (PRODUCTION) {
       // Point to prod sayonara
-      sayonaraUrlHost = '//' + this.$location.host() + this.$location.port();
+      if (this.$location.port()) {
+        sayonaraUrlHost = '//' + this.$location.host() + ':' + this.$location.port();
+      } else {
+        sayonaraUrlHost = '//' + this.$location.host();
+      }
     } else {
       // Point to Dev sayonara
       sayonaraUrlHost = API_URL;
